ee76948785924b420db2503e45b9fe280be8b5e0
jonghunAn <dkswhdgnsvv@gmail.com>
2021-12-29 17:14:00 +0900

    노드모듈 삭제

--- src/components/home/Card.js
+++ src/components/home/Card.js
@@ -99,9 +99,7 @@ function Card({ number, images, author, date, text }) {
   const imgSlideClick = async (e) => {
     const imgNum = e.target.value; //이미지 번호
     let mainImg = document.getElementById(`mainImg${number}`);
-    mainImg.src = `http://localhost:8030/write/test?image=${
-      images[0][`img${Number(imgNum) + 1}`]
-    }`;
+    mainImg.src = `./write/test?image=${images[0][`img${Number(imgNum) + 1}`]}`;
   };
 
   const authorClick = (author) => {
@@ -127,7 +125,7 @@ function Card({ number, images, author, date, text }) {
     <CardWrapper>
       <ImageSlide>
         <img
-          src={`http://localhost:8030/write/test?image=${images[0].img1}`}
+          src={`./write/test?image=${images[0].img1}`}
           alt="alt"
           id={`mainImg${number}`}
           height="250px"
